749222ed0b7311d6853cdb3124cd8c4262a3aad8
Ablrt <albert_amadanyan@edu.aua.am>
2023-09-28 18:56:54 +0400

    send message to tg if new chapter

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from os.path import join, dirname
+from dotenv import load_dotenv
+import os
 import json
 import time
+import logging
+import requests
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+TOKEN = os.environ.get("BOT_TOKEN")
+ID = os.environ.get("CHAT_ID")
+
+INTERVAL_IN_SECONDS = 300
 
 def get_latest_chapter(driver, series_name):
     element = driver.find_element(By.PARTIAL_LINK_TEXT, series_name)
@@ -33,6 +46,7 @@ def main():
     for name, chapter in latest_chapters.items():
         if chapter != chapters.get(name):
             chapters[name] = chapter
+            notify(name,chapter)
             updated = True
 
     if updated:
@@ -41,5 +55,18 @@ def main():
     end = time.time()
     print(end -start)
 
+
+def notify(title: str, chapter:str):
+    apiURL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    message = f'new chapter {chapter} for {title} has dropped!'
+
+    try:
+        response = requests.post(apiURL, json={'chat_id': ID, 'text': message})
+        logging.info(response.text)
+    except Exception as e:
+        logging.error(e)
+
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
+        time.sleep(INTERVAL_IN_SECONDS)
