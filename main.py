97d6fd9d39b039565820d7a53f41f546a85e2213
Ablrt <albert_amadanyan@edu.aua.am>
2023-10-06 17:07:06 +0400

    exception handling in case of scraping fail

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ import time
 
 INTERVAL_IN_SECONDS = 120
 
-def get_latest_chapter(driver, series_name):
-    element = driver.find_element(By.PARTIAL_LINK_TEXT, series_name)
-    return element.text.rsplit(' ', 1)[1]
+def get_latest_chapter(driver, series_name, chapter):
+    try:
+        element = driver.find_element(By.PARTIAL_LINK_TEXT, series_name)
+        return element.text.rsplit(' ', 1)[1]
+    except Exception as ex:
+        print(ex)
+        return chapter
 
 def main():
     start = time.time()
@@ -18,17 +22,16 @@ def main():
     options = Options()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
     options.add_argument('--headless')
+    chapters = get_chapters()
 
     with webdriver.Chrome(options=options) as driver:
         driver.get(link)
 
         latest_chapters = {
-            'MHA': get_latest_chapter(driver, "My Hero"),
-            'JJK': get_latest_chapter(driver, "Jujutsu "),
-            'OP': get_latest_chapter(driver, "One Piece")
+            'MHA': get_latest_chapter(driver, "My Hero", chapters["MHA"]),
+            'JJK': get_latest_chapter(driver, "Jujutsu", chapters["JJK"]),
+            'OP': get_latest_chapter(driver, "One Piece", chapters["OP"])
         }
-
-    chapters = get_chapters()
 
     for name, chapter in latest_chapters.items():
         if chapter != chapters.get(name):
